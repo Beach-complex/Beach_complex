da60ae73ad8768c7a074120fa178dfa56dbb6561
GunWoo Park <rjsdn7895@gnu.ac.kr>
2026-04-06 19:48:36 +0900

    test: PB-104 AwsSigV4InterceptorTest 케이스 추가
    - TC1-1: 빈 GET 요청에서 SignedHeaders에 content-length가 포함되지 않는지 검증
    - TC3: 세션 자격증명 사용 시 X-Amz-Security-Token 헤더가 추가되는지 검증

--- src/test/java/com/beachcheck/client/AwsSigV4InterceptorTest.java
+++ src/test/java/com/beachcheck/client/AwsSigV4InterceptorTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,7 @@ import org.springframework.mock.http.client.MockClientHttpRequest;
 import org.springframework.mock.http.client.MockClientHttpResponse;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.core.exception.SdkClientException;
 
 @SuppressWarnings("resource") // MockClientHttpResponse는 byte[] 래퍼로 close()가 no-op
@@ -47,7 +49,7 @@ class AwsSigV4InterceptorTest {
   }
 
   @Test
-  @DisplayName("TC1 - 서명 후 실행된 요청에 Authorization, X-Amz-Date 헤더가 추가된다")
+  @DisplayName("TC1 - 서명 후 실행된 요청에 Authorization, X-Amz-Date 헤더가 추가되고 scope가 맞는다")
   void shouldAddSigV4Headers() throws IOException {
     // Given
     MockClientHttpRequest request = new MockClientHttpRequest(HttpMethod.GET, LAMBDA_URI);
@@ -61,8 +63,30 @@ class AwsSigV4InterceptorTest {
     ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
     then(execution).should().execute(captor.capture(), any());
     HttpRequest signedRequest = captor.getValue();
-    assertThat(signedRequest.getHeaders()).containsKey("Authorization");
+    assertThat(signedRequest.getHeaders()).containsKey(HttpHeaders.AUTHORIZATION);
     assertThat(signedRequest.getHeaders()).containsKey("X-Amz-Date");
+    assertThat(signedRequest.getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+        .contains("/" + REGION + "/lambda/aws4_request");
+  }
+
+  @Test
+  @DisplayName("TC1-1 - 빈 GET 요청은 SignedHeaders에 content-length를 포함하지 않는다")
+  void shouldNotSignContentLength_forEmptyGetRequest() throws IOException {
+    // Given
+    MockClientHttpRequest request = new MockClientHttpRequest(HttpMethod.GET, LAMBDA_URI);
+    request.getHeaders().setContentLength(0);
+    given(execution.execute(any(), any()))
+        .willReturn(new MockClientHttpResponse(new byte[] {}, HttpStatus.OK));
+
+    // When
+    interceptor.intercept(request, new byte[] {}, execution);
+
+    // Then
+    ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+    then(execution).should().execute(captor.capture(), any());
+    HttpRequest signedRequest = captor.getValue();
+    assertThat(signedRequest.getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+        .doesNotContain("content-length");
   }
 
   @Test
@@ -76,5 +100,31 @@ class AwsSigV4InterceptorTest {
     // When & Then
     assertThatThrownBy(() -> interceptor.intercept(request, new byte[] {}, execution))
         .isInstanceOf(SdkClientException.class);
+  }
+
+  @Test
+  @DisplayName("TC3 - 세션 자격증명 사용 시 X-Amz-Security-Token 헤더가 추가된다")
+  void shouldAddSecurityToken_whenSessionCredentialsUsed() throws IOException {
+    // Given
+    given(credentialsProvider.resolveCredentials())
+        .willReturn(
+            AwsSessionCredentials.create(
+                "test-access-key", "test-secret-key", "test-session-token"));
+    interceptor = new AwsSigV4Interceptor(credentialsProvider, REGION);
+
+    MockClientHttpRequest request = new MockClientHttpRequest(HttpMethod.GET, LAMBDA_URI);
+    given(execution.execute(any(), any()))
+        .willReturn(new MockClientHttpResponse(new byte[] {}, HttpStatus.OK));
+
+    // When
+    interceptor.intercept(request, new byte[] {}, execution);
+
+    // Then
+    ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+    then(execution).should().execute(captor.capture(), any());
+    HttpRequest signedRequest = captor.getValue();
+    assertThat(signedRequest.getHeaders()).containsKey("X-Amz-Security-Token");
+    assertThat(signedRequest.getHeaders().getFirst("X-Amz-Security-Token"))
+        .isEqualTo("test-session-token");
   }
 }
